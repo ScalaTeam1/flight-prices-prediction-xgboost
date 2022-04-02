a1752d05d956b73b93d9225a320c4d34c48a76eb
channel960608 <997476274@qq.com>
2022-04-01 21:56:15 -0400

    FIX: issues

--- src/main/scala/com/neu/edu/demo.scala
+++ src/main/scala/com/neu/edu/demo.scala
@@ -1,10 +1,11 @@
 package com.neu.edu
 
-import ml.dmlc.xgboost4j.scala.spark.{TrackerConf, XGBoostClassificationModel, XGBoostRegressor}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import ml.dmlc.xgboost4j.scala.spark.{TrackerConf, XGBoostRegressionModel, XGBoostRegressor}
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.ml.{Pipeline, PipelineModel, PipelineStage}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
@@ -21,8 +22,8 @@ object demo {
       .appName("xiangdangdang")
       .config("spark.master", "local")
       .getOrCreate()
-    val inputPath = "/Users/arronshentu/Downloads/final/dataset/Clean_Dataset.csv"
-    val modelPath = "/Users/arronshentu/Downloads/final/model"
+    val inputPath = "./dataset/Clean_Dataset.csv"
+    val modelPath = "./model"
 
     val schema = new StructType(Array(
       StructField("id", IntegerType, nullable = true),
@@ -43,9 +44,10 @@ object demo {
 
     val pipeline = new Pipeline()
     // pipelines
-    val categoricalCols = Array("airline", "source_city", "departure_time", "stops", "arrival_time", "destination_city", "class")
+    val oneHotFeatures = Array("airline", "source_city", "departure_time", "stops", "arrival_time", "destination_city", "class")
+    val numericFeatures = Array("duration", "days_left")
     val stagesArray = new ListBuffer[PipelineStage]()
-    for (cate <- categoricalCols) {
+    for (cate <- oneHotFeatures) {
       val indexer = new StringIndexer()
         .setInputCol(cate)
         .setOutputCol(s"${cate}Index")
@@ -54,7 +56,8 @@ object demo {
       stagesArray.append(indexer, encoder)
     }
 
-    val mappedCategoricalCols = categoricalCols.map(cate => s"${cate}classVec")
+
+    val mappedCategoricalCols = oneHotFeatures.map(cate => s"${cate}classVec")
 
     // Build ML pipeline, it includes 4 stages:
     // 1, Assemble all features into a single vector column.
@@ -62,12 +65,12 @@ object demo {
     // 3, Use XGBoostClassifier to train classification model.
     // 4, Convert indexed double label back to original string label.
     val assembler = new VectorAssembler().
-      setInputCols(mappedCategoricalCols).
+      setInputCols(mappedCategoricalCols ++ numericFeatures).
       setOutputCol("features")
     val trackerConf = new TrackerConf(0, "scala")
     val regressor = new XGBoostRegressor(
       Map("eta" -> 0.1f,
-        "max_depth" -> 2,
+        "max_depth" -> 8,
         "objective" -> "reg:squarederror",
         "num_class" -> 3,
         "num_round" -> 50,
@@ -86,7 +89,7 @@ object demo {
     pipeline.setStages(stagesArray.toArray)
 
     // split
-    val Array(training, test) = rawInput.randomSplit(Array(0.4, 0.2))
+    val Array(training, test) = rawInput.randomSplit(Array(0.8, 0.2)) // Split training and test set with radio 4/1
     println(s"trainingDF size=${training.count()},testDF size=${test.count()}")
     // train
     val model = pipeline.fit(training)
@@ -94,12 +97,11 @@ object demo {
     prediction.show(false)
 
 
-    val evaluator = new MulticlassClassificationEvaluator()
+    val evaluator = new RegressionEvaluator() // Have to use regression evaluator rather than classification evaluator
     evaluator.setLabelCol("price")
     evaluator.setPredictionCol("prediction")
-    val accuracy = evaluator.evaluate(prediction)
-    println("The model accuracy is : " + accuracy)
-
+    val score = evaluator.evaluate(prediction)
+    println("The model regression evaluation score is : " + score)
 
     // Tune model using cross validation
     val paramGrid = new ParamGridBuilder()
@@ -113,12 +115,12 @@ object demo {
       .setNumFolds(3)
 
     val cvModel = cv.fit(training)
-
-    val bestModel = cvModel.bestModel.asInstanceOf[PipelineModel].stages(2)
-      .asInstanceOf[XGBoostClassificationModel]
-    println("The params of best XGBoostClassification model : " +
+    // It's regression model rather than classification model
+    val bestModel = cvModel.bestModel.asInstanceOf[PipelineModel].stages(15)
+      .asInstanceOf[XGBoostRegressionModel]
+    println("The params of best XGBoostRegressionModel : " +
       bestModel.extractParamMap())
-    println("The training summary of best XGBoostClassificationModel : " +
+    println("The training summary of best XGBoostRegressionModel : " +
       bestModel.summary)
 
 
